854332039c2e332660081d8d6dda19f07d27b5c6
D'Artagnan von Pingel <dvonpingel@gmail.com>
2021-01-25 13:28:47 -0700

    Final Commit

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+import { Link, useParams, useRouteMatch } from 'react-router-dom'
+
 export default function Movie(props) {
   const [movie, setMovie] = useState();
 
-  let id = 1;
+
+  let { url } = useRouteMatch();
+
+  let { id } = useParams();
   // Change ^^^ that line and use a hook to obtain the :id parameter from the URL
+
 
   useEffect(() => {
     axios
@@ -13,13 +19,14 @@ export default function Movie(props) {
       .then(response => {
         // Study this response with a breakpoint or log statements
         // and set the response data as the 'movie' slice of state
+        setMovie(response.data);
       })
       .catch(error => {
         console.error(error);
       });
     // This effect should run every time time
     // the `id` changes... How could we do this?
-  }, []);
+  }, [id]);
 
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = evt => { }
@@ -32,22 +39,22 @@ export default function Movie(props) {
 
   return (
     <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
+        <div className="movie-card">
+          <h2>{title}</h2>
+          <div className="movie-director">
+            Director: <em>{director}</em>
           </div>
-        ))}
-      </div>
+          <div className="movie-metascore">
+            Metascore: <strong>{metascore}</strong>
+          </div>
+          <h3>Actors</h3>
+
+          {stars.map(star => (
+            <div key={star} className="movie-star">
+              {star}
+            </div>
+          ))}
+        </div>
       <div className="save-button">Save</div>
     </div>
   );
